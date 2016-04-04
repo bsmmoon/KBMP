c2972ed07db6aa1608e8f1d891e6c4bca88c17a8
Joey C <joeycyq@gmail.com>
2016-04-04 19:12:05 +0800

    Populate with one semester of modules

--- KBMP/src/backend/Model.java
+++ KBMP/src/backend/Model.java
@@ -2,21 +2,46 @@ package backend;/*
  * Facet
  */
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 import common.Module;
 
 public class Model {
 	private ClipsWrapper clips;
-	
-	private ArrayList<Module> availableModules;
+	private ArrayList<Module> availableModules = populateModules();
 
 	public Model(ClipsWrapper clips) {
 		this.clips = clips;
-		this.availableModules = new ArrayList<Module>();
+		this.availableModules = new ArrayList<>();
 	}
 
 	public ArrayList<Module> getAvailableModules() { return availableModules; }
 
 	public void update() { availableModules = clips.getAvailableModules(); }
+
+	public ArrayList<Module> populateModules() {
+		ArrayList<Module> modules = new ArrayList<>();
+		// if file containing collated modules exists, just populate from file
+
+		// else if raw files exist, generate collated modules
+		Path sem1ModulesJson = Paths.get("data/AY1516_S1_modules.json");
+		// Path sem2ModulesJson = Paths.get("data/AY1516_S2_modules.json");
+		try {
+//			ArrayList<Module> sem1 = ModulesParser.updateModulesFromPath(sem1ModulesJson, new Hashtable<>(),
+//					Module.Semester.ONE);
+			return ModulesParser.updateModulesFromPath(sem1ModulesJson, new Hashtable<>(), Module.Semester.ONE);
+		} catch (IOException e) {
+			return modules;
+		}
+
+		// else throw error
+
+//		return modules;
+	}
 }
