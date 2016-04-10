57e06b8c2048ecbbc9fde720ddfbcfe31425451f
bsmmoon <bsmmoon@gmail.com>
2016-04-11 01:35:13 +0800

    Prerequisite parsing

--- KBMP/src/backend/ClipsParser.java
+++ KBMP/src/backend/ClipsParser.java
@@ -9,20 +9,14 @@ import java.util.ArrayList;
  * Needs to support bidirectional parsing (Java <-> CLIPS)
  */
 public class ClipsParser {
-	public static String parseModuleIntoClips(Module module) {
+	public static ArrayList<String> parseModuleIntoClips(Module module) {
+		ArrayList<String> result = new ArrayList<>();
+
 		String out;
 		out = "(assert (module " +
 				"(code \"" + module.getCode() +"\")" +
 				"(name \"" + module.getName() +"\")" +
 				"(MC " + module.getCredits() + ")";
-//		if (module.getPrerequisites().size() > 0) {
-//			out += "(prerequisites";
-//			for (String prerequisite : module.getPrerequisites()) {
-//				out += " \"" + prerequisite + "\"";
-//			}
-//			out += ")";
-//		}
-
 		String code = module.getCode();
 		for (int i = 0; i < code.length(); i++) {
 			int c = code.charAt(i);
@@ -34,8 +28,21 @@ public class ClipsParser {
 			}
 		}
 
-		out += "))";
-		return out;
+		ArrayList<String> prereqCombinations = parsePrereq(module.getPrerequisites());
+		if (prereqCombinations.size() > 0) {
+			String combination;
+			for (String prereq : prereqCombinations) {
+				combination = out;
+				combination += prereq;
+				combination += "))";
+				result.add(combination);
+			}
+		} else {
+			out += "))";
+			result.add(out);
+		}
+
+		return result;
 	}
 
 	public static String parseFocusAreaIntoClips(FocusArea focusArea) {
@@ -64,5 +71,51 @@ public class ClipsParser {
 		out += "))";
 
 		return out;
+	}
+
+	private static ArrayList<String> parsePrereq(String prereq) {
+		ArrayList<String> result = new ArrayList<>();
+		prereq = prereq.trim();
+		if (prereq.isEmpty()) {
+			return result;
+		}
+
+		ArrayList<String[]> list = new ArrayList<>();
+		int from, to;
+		while (prereq.contains("(")) {
+			from = prereq.indexOf("(");
+			to = prereq.indexOf(")");
+			list.add(prereq.substring(from+1, to).split(" or "));
+			prereq = prereq.replace(prereq.substring(from, to+1), "");
+		}
+		String[] arr = prereq.split(" and ");
+		if (arr.length == 1 && list.isEmpty()) {
+			list.add(arr[0].split(" or "));
+			arr = new String[0];
+		}
+
+		for (String str : arr) {
+			if (str.isEmpty()) continue;
+			list.add(new String[]{str});
+		}
+
+		result = combination(list);
+		return result;
+	}
+
+	private static ArrayList<String> combination(ArrayList<String[]> list) {
+		ArrayList<String> result = new ArrayList<>();
+		combinationRecurr(list, 0, "", result);
+		return result;
+	}
+
+	private static void combinationRecurr(ArrayList<String[]> list, int index, String command, ArrayList<String> result) {
+		if (index == list.size()) {
+			result.add("(prerequisites" + command + ")");
+			return;
+		}
+		for (String str : list.get(index)) {
+			combinationRecurr(list, index + 1, command + " \"" + str + "\"", result);
+		}
 	}
 }
